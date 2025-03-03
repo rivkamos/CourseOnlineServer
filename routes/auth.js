61d386e450f64d73d9dccbb0456debb08ca90c84
rivkamos <77576647+rivkamos@users.noreply.github.com>
2025-03-03 11:45:31 +0200

    change registration api

--- routes/auth.js
+++ routes/auth.js
@@ -18,7 +18,7 @@ module.exports = (db) => {
         }
         const token = jwt.sign({ userId: userId, role: role }, 'secret');
         console.log('User registered successfully:', userId, token);
-        res.status(201).json({ message: 'User registered successfully', userId , token});
+        res.status(201).json({ message: 'User registered successfully', userId , token, role});
       });
     } catch (err) {
       res.status(500).json({ message: 'Error registering user' });
